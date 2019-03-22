0f6f42e0fe8ae12325ee1491cefa8f346cc1e458
iankii <ankit.pal2011@gmail.com>
2019-03-22 21:57:50 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    // we can use either eaxct or switch
+    return (
+      <div>
+        Testing...
+      </div>
+    );
+  }
+}
+
+export default App;
