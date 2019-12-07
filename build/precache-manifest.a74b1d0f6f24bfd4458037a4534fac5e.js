03c0d6a51b897c604c009614dc95cc532a90f618
iankii <ankit.pal2011@gmail.com>
2019-12-07 19:20:00 +0530

    asdfg

---
+++ build/precache-manifest.a74b1d0f6f24bfd4458037a4534fac5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b946fd1cb83e7b18657e671dd5b745d",
+    "url": "/static/media/prof.2b946fd1.webp"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f41ec5e7bbd4810ca263",
+    "url": "/static/js/main.f41ec5e7.chunk.js"
+  },
+  {
+    "revision": "3d353a99e48e39fee8b5",
+    "url": "/static/js/1.3d353a99.chunk.js"
+  },
+  {
+    "revision": "f41ec5e7bbd4810ca263",
+    "url": "/static/css/main.ac27edc2.chunk.css"
+  },
+  {
+    "revision": "d04defd68b6cd64cf746a6948c31a8f9",
+    "url": "/index.html"
+  }
+];
